a5db25c9fee2767b144f3015003b0f31363c7447
Chakotay Incorvaja <chakotay.incorvaja@gmail.com>
2019-12-11 16:44:32 +0100

    Removed .idea files, added gitignore and 70% of feedforward

--- Main.py
+++ Main.py
@@ -35,21 +35,62 @@ trainingSet = np.array([["00000", "100"],
                         ["11111", "011"],
                         ])
 
-# Weights
-wIn = np.zeros(5, 4)
-wOut = np.zeros(4, 3)
+# Weights random assignment between -1 and 1
+wIn = np.random.uniform(low=-1, high=1, size=(5, 4))
+wOut = np.random.uniform(low=-1, high=1, size=(4, 3))
 
 # Target and Input
 input = np.array([0, 0, 0, 0, 0])
 target = np.array([1, 0, 0])
 
-# These are for the input - outH is the sigmoid of netH
-netH = np.zero(1, 4)
-outH = np.zero(1, 4)
+# Input - outH is the sigmoid of netH
+# netH = np.zeros((1, 4))
+# outH = np.zeros((1, 4))
 
-# These are for the output - outO is the sigmoid of netO
-netO = np.zero(1, 3)
-outO = np.zero(1, 3)
+# Output - outO is the sigmoid of netO
+# netO = np.zeros((1, 3))
+# outO = np.zeros((1, 3))
 
-def initilizeWeights(weight):
+# Error storage
+errorStore = np.array([0, 0, 0], dtype=np.float64)
 
+
+def setInputAndTarget(tSet, inputMatrix, targetMatrix):
+    inputString = tSet[31, 0]
+    targetString = tSet[0, 1]
+    i = 0
+    x = 0
+    for aNumber in inputString:
+        inputMatrix[i] = aNumber
+        i = i + 1
+    for aNumber in targetString:
+        targetMatrix[x] = aNumber
+        x = x + 1
+
+
+def feedforward(errorList):
+    netH = np.dot(input, wIn).astype(np.float64)
+    outH = sigmoid(netH)
+    netO = np.dot(outH, wOut).astype(np.float64)
+    outO = sigmoid(netO)
+    i = 0
+    for eachNumber in outO:
+        newNum = np.subtract(target[i], eachNumber).astype(np.float64)
+        errorList[i] = newNum
+        i = i + 1
+
+    print("netH:\n", netH)
+    print("outH:\n", outH)
+    print("netO:\n", netO)
+    print("outO:\n", outO)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+setInputAndTarget(trainingSet, input, target)
+print("Input:\n", input)
+print("Output:\n", target)
+feedforward(errorStore)
+print("Error Numbers:\n", errorStore)
