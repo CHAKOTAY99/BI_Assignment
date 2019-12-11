71ea6f2a4759fab0bfe990d18f751e9f8f2b0d03
Chakotay Incorvaja <chakotay.incorvaja@gmail.com>
2019-12-11 23:46:24 +0100

    Improved and centralized into one main method

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Training Set 2^5 = 32 ABCDE = ¬ACD #
+# Training Set 2^5 = 32 ABCDE = ¬ACD # : 80% of 32 = 26, 20% = 6
 trainingSet = np.array([["00000", "100"],
                         ["00001", "100"],
                         ["00010", "101"],
@@ -40,24 +40,28 @@ wIn = np.random.uniform(low=-1, high=1, size=(5, 4))
 wOut = np.random.uniform(low=-1, high=1, size=(4, 3))
 
 # Target and Input
-input = np.array([0, 0, 0, 0, 0])
-target = np.array([1, 0, 0])
+# input = np.array([0, 0, 0, 0, 0])
+# target = np.array([1, 0, 0])
 
 # Input - outH is the sigmoid of netH
-# netH = np.zeros((1, 4))
-# outH = np.zeros((1, 4))
+netH = np.zeros((1, 4)).astype(np.float64)
+outH = np.zeros((1, 4)).astype(np.float64)
 
 # Output - outO is the sigmoid of netO
-# netO = np.zeros((1, 3))
-# outO = np.zeros((1, 3))
+netO = np.zeros((1, 3)).astype(np.float64)
+outO = np.zeros((1, 3)).astype(np.float64)
 
 # Error storage
 errorStore = np.array([0, 0, 0], dtype=np.float64)
 
+# Facts per epoch storage; epoc number,good fact, bad fact
+epochStorage = np.array([0, 0, 0])
 
-def setInputAndTarget(tSet, inputMatrix, targetMatrix):
+def setInputAndTarget(tSet):
     inputString = tSet[31, 0]
     targetString = tSet[0, 1]
+    inputMatrix = np.array([0, 0, 0, 0, 0])
+    targetMatrix = np.array([0, 0, 0])
     i = 0
     x = 0
     for aNumber in inputString:
@@ -66,13 +70,17 @@ def setInputAndTarget(tSet, inputMatrix, targetMatrix):
     for aNumber in targetString:
         targetMatrix[x] = aNumber
         x = x + 1
+    print("input\n", inputMatrix)
+    print("target\n", targetMatrix)
+    return inputMatrix, targetMatrix
 
 
-def feedforward(errorList):
-    netH = np.dot(input, wIn).astype(np.float64)
+def feedforward(feed, goal):
+    netH = np.dot(feed, wIn).astype(np.float64)
     outH = sigmoid(netH)
     netO = np.dot(outH, wOut).astype(np.float64)
     outO = sigmoid(netO)
+    errorList = [0, 0, 0]
     i = 0
     for eachNumber in outO:
         newNum = np.subtract(target[i], eachNumber).astype(np.float64)
@@ -83,14 +91,26 @@ def feedforward(errorList):
     print("outH:\n", outH)
     print("netO:\n", netO)
     print("outO:\n", outO)
+    print("Error Numbers:\n", errorList)
+
+    return outO, errorList
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+#
+# def mainFunction():
+#     while True:
+#         for eachSample in trainingSet:
+#             setInputAndTarget(eachSample, input, target)
+#
+#
 
-setInputAndTarget(trainingSet, input, target)
-print("Input:\n", input)
-print("Output:\n", target)
-feedforward(errorStore)
-print("Error Numbers:\n", errorStore)
+
+itResult = setInputAndTarget(trainingSet)
+input = itResult[0]
+target = itResult[1]
+result = feedforward(input, target)
+
+print("Facts:\n", epochStorage)
